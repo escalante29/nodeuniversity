0c657f543d9c73e850d3165a3484fa45525fd7c7
Carlos Escalante <carlos.escalante.cr@outlook.com>
2018-01-02 10:11:09 -0600

    added subfolder and mongodb connection data

---
+++ node_mongodb_foundation/app.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const app = require('express');
+const dbURI = 'mongodb://admin:mongo18@localhost:27017/users?authenticationDatabase=admin';
+
+// Connect to our Database and handle an bad connections
+mongoose.connect(dbURI);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// Start our app!
+const app = require('./app');
+const port =  process.env.PORT || 80;
+console.log(`port set to: ${port}`);
+app.set('port', port);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${port}`);
+});
